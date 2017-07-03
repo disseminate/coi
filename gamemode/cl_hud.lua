46e04af534fd78e58bf5eaf53808bdd4f3f732d2
Kyle Windsor <kwindsor@uwo.ca>
2017-07-03 15:11:29 -0400

    :sparkles: HUD removal

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -1,1 +1,14 @@
---
+local HUDElements = {
+	"CHudAmmo",
+	"CHudBattery",
+	"CHudHealth",
+	"CHudWeaponSelection"
+};
+
+function GM:HUDShouldDraw( name )
+
+	if( table.HasValue( HUDElements, name ) ) then return false; end
+
+	return self.BaseClass:HUDShouldDraw( name );
+
+end
