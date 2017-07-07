5d6785956c055dc5275fcdb49892117d0d330f91
Kyle Windsor <kwindsor@uwo.ca>
2017-07-07 14:16:36 -0400

    :sparkles: Team names

--- gamemode/sh_state.lua
+++ gamemode/sh_state.lua
@@ -117,15 +117,61 @@ function GM:ResetMapTrucks()
 
 end
 
+local teamNames = {
+	"Lion",
+	"Crocodile",
+	"Bear",
+	"Stallion",
+	"Wasp",
+	"Cobra",
+	"Mosquito",
+	"Scorpion",
+	"Union",
+	"Sexsmith",
+	"Lambda",
+	"Omega",
+	"Hunter",
+	"Blade",
+	"Scar",
+	"Hammer",
+	"Fist",
+	"Razor",
+	"Stab",
+	"Dagger",
+	"Reaper",
+	"Nomad",
+	"Star",
+	"Phantom",
+	"12th Street"
+};
+
+local gangNames = {
+	"Crew",
+	"Team",
+	"Squad",
+	"Family",
+	"Saints",
+	"Kings",
+	"Mafia",
+	"Mob",
+	"Circle",
+	"Nation"
+};
+
 function GM:InitializeTeams()
 
 	self:ResetMapTrucks();
 
+	-- New team name per day.
+	math.randomseed( 1499450893 + tonumber( os.date( "%j", math.floor( os.time() - CurTime() ) ) ) );
+
 	for k, v in pairs( self.Teams ) do
 
-		team.SetUp( v, "Crew #" .. v, HSVToColor( ( v - 1 ) * 70, 0.5, 1 ) );
+		team.SetUp( v, table.Random( teamNames ) .. " " .. table.Random( gangNames ), HSVToColor( ( v - 1 ) * 70, 0.5, 1 ) );
 
 	end
+
+	math.randomseed( os.time() );
 
 	team.SetUp( TEAM_UNJOINED, "Unjoined", Color( 128, 128, 128 ), false );
 
