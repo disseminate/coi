55fa2fdf6e3eb9ea241a5745a86885a712211bd1
Kyle Windsor <kyle@kylewindsor.io>
2017-07-16 21:10:34 -0400

    :speech_balloon: Updated trip/prox descriptions

--- gamemode/items/proxmine.lua
+++ gamemode/items/proxmine.lua
@@ -1,7 +1,7 @@
 EXPORTS["proxmine"] = { };
 
 EXPORTS["proxmine"].Name = "Proximity Mine";
-EXPORTS["proxmine"].Desc = "Take a guess.\n\nOnce used, you can get more at your truck.";
+EXPORTS["proxmine"].Desc = "Detonates when it detects movement nearby.\n\nOnce used, you can get more at your truck.";
 EXPORTS["proxmine"].Secondary = true;
 EXPORTS["proxmine"].Price = 10000;
 EXPORTS["proxmine"].Model = "models/weapons/w_slam.mdl";
